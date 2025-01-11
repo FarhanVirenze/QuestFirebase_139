da307df5b8e75dd2e7a9398e270f88a9c4ab1db0
farhan <farhanvirenze18@gmail.com>
2025-01-11 20:29:33 +0700

    Membuat MainActivity

---
+++ app/src/main/java/com/pam/pamfirebase_139/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.pam.pamfirebase_139
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.pam.pamfirebase_139.ui.theme.PAMFIREBASE_139Theme
+import com.pam.pamfirebase_139.ui.viewmodel.MahasiswaApp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PAMFIREBASE_139Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    MahasiswaApp(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PAMFIREBASE_139Theme {
+        Greeting("Android")
+    }
+}
